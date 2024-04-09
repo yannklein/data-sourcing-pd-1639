62724aba796ccf705ae95677f46d945d668f410f
Yann Klein <yann.klein@me.com>
2024-04-09 10:59:56 +0900

    lecture done

--- .ipynb_checkpoints/music-checkpoint.py
+++ .ipynb_checkpoints/music-checkpoint.py
@@ -1,14 +1,13 @@
 import requests
 
-def fetch_lyrics(artist_name, song):
-    url = 'https://lyrics.lewagon.ai/search'
+def fetch_lyrics(artist, title):
     try:
+        url = f"https://lyrics.lewagon.ai/search"
         response = requests.get(url, params={
-            'artist': artist_name,
-            'title': song
+            'artist': artist,
+            'title': title
         })
-        # response.status_code is 200 or 404?
         data = response.json()
         return data['lyrics']
     except:
-        return 'No data'
+        return 'No lyrics'
